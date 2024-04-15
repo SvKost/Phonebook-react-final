5e48f6902511bd6d120e807123268ed96c8061e4
SvKost <skost0634610557@gmail.com>
2024-04-15 16:43:36 +0300

    styles added & fixed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,27 @@
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <div>
       <title>Home Page</title>
-      You can see your contacts in the Phonebook
+      <h3>You are in the Contact organiser.</h3>
+      {!isLoggedIn && (
+        <p>
+          To see and add contacts, please{" "}
+          <Link to="/login">
+            <b>Log in</b>
+          </Link>{" "}
+          or{" "}
+          <Link to="/register">
+            <b>Register</b>
+          </Link>
+          !
+        </p>
+      )}
     </div>
   );
 };
