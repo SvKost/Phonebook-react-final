4b3af0e29377f22013016962602ae94027677f2c
SvKost <skost0634610557@gmail.com>
2024-04-16 10:01:13 +0300

    Added conditional rendering

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,20 +1,25 @@
 import { NavLink } from "react-router-dom";
 import css from "./Navigation.module.css";
 import clsx from "clsx";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const buildLinkClass = ({ isActive }) => {
   return clsx(css.link, isActive && css.active);
 };
 
 const Navigation = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
     <nav className={css.nav}>
       <NavLink to="/" className={buildLinkClass}>
         Home
       </NavLink>
-      <NavLink to="/contacts" end className={buildLinkClass}>
-        Contacts
-      </NavLink>
+      {isLoggedIn && (
+        <NavLink to="/contacts" end className={buildLinkClass}>
+          Contacts
+        </NavLink>
+      )}
     </nav>
   );
 };
